75c91f3600e8119cb924a4faa179ab3b7e7a236a
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-31 21:16:11 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,11 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+      super();
+      this.state = {
+        reviews: [],
+        searchTerm: "",
+      };
+    }
