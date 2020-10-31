b94482f113f0f3bdd7460d978d14f67405ab146b
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-31 21:10:34 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,3 +1,7 @@
 // Code MovieReviews He
 import React from "react";
 
+const MovieReviews = (props) => {
+  const renderReviews = () => {
+    return props.reviews.map((movieReview, index) => (
+      <div className="review" key={movieReview.display_title + index}>
